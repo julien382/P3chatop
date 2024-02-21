3d487dcabc9cdf91d792f806db1fb70cd4973811
julien382 <julhermain@gmail.com>
2024-02-21 15:55:35 +0100

    save

--- src/main/java/com/openclassrooms/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/configuration/SpringSecurityConfig.java
@@ -31,7 +31,11 @@ public class SpringSecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/register", "/api/auth/login").permitAll().anyRequest().authenticated())
+				.authorizeHttpRequests(auth -> auth.requestMatchers(
+														"/api/auth/register", 
+																	"/api/auth/login"
+																	)
+				.permitAll().anyRequest().authenticated())
 				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
 				.httpBasic(Customizer.withDefaults()).build();
 	}
